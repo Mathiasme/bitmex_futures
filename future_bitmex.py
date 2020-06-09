86692477920fc91f7c005eb7f49f9f85e470c653
mathiasme <38974495+Mathiasme@users.noreply.github.com>
2020-06-09 10:08:37 +0200

    Update future_bitmex.py

--- future_bitmex.py
+++ future_bitmex.py
@@ -1,58 +1,60 @@
+#Not financial advice
 # -*- coding:UTF-8 -*-
 import psycopg2
 
-conn = psycopg2.connect(database="final", user="postgres",password="xxxxxxx", host="127.0.0.1", port="5432")
-print("ça marche connexion réussie")
+conn = psycopg2.connect(database="db_name", user="postgres",password="xxxxxxx", host="127.0.0.1", port="5432")
+print("You are connected to the database")
 
-cursor=conn.cursor()
+cursor = conn.cursor()
 
-listeBingo=[]
-liquidation=int(0)
-NbreDeJoursMacro=0
-dateDepartFixe="2012-11-28"
+listeBingo = []
+liquidation = int(0)
+NbreDeJoursMacro = 0
+dateDepartFixe = "2012-11-28"
 
 def funcLevier(levier):
-    if levier==3:
-        return 0.85
-    elif levier==5:
-        return 0.90
-    elif levier==10:
-        return 0.95
-    elif levier==20:
-        return 0.98
-    elif levier==100:
-        return 0.999
-    else:
-        print("Vous avez choisi un mauvais levier")
+    switch(levier){
+        case 3:
+            return 0.85
+        case 5:
+            return 0.90
+        case 10:
+            return 0.95
+        case 20:
+            return 0.98
+        case 100:
+            return 0.999
+        default:
+         print("You picked a leverage that is not allowed")
 
-def jeff(NbreDeJoursMacro):
-    a=float(1.00)
-    NbreDeJoursMicro=int(0)
+def mainFunc(NbreDeJoursMacro):
+    a = float(1.00)
+    NbreDeJoursMicro = int(0)
     #print(liquidation)
-    while (a>liquidation) and (NbreDeJoursMicro<180):
-        sommeJours=int(NbreDeJoursMacro+NbreDeJoursMicro)
+    while (a > liquidation) and (NbreDeJoursMicro < 180):
+        sommeJours = int(NbreDeJoursMacro+NbreDeJoursMicro)
         cursor.execute("SELECT percentage FROM public.bitmexx WHERE datee - integer '%s' = %s;",(sommeJours, dateDepartFixe))
-        records=cursor.fetchall()
-        records=records[0]
-        records=str(records)
-        records=records.replace("(","")
-        records=records.replace(",","")
-        records=records.replace(")","")
-        records=float(records)
-        a=(1+(records/100))*a
-        #print("Le contrat Future a tenu ",NbreDeJoursMicro," jours et a=",a)
-        x=NbreDeJoursMicro
-        y=a
-        NbreDeJoursMicro=NbreDeJoursMicro+1
-    if x==179:
+        records = cursor.fetchall()
+        records = records[0]
+        records = str(records)
+        records = records.replace("(","")
+        records = records.replace(",","")
+        records = records.replace(")","")
+        records = float(records)
+        a = (1+(records/100))*a
+        #print("the future contract held ",NbreDeJoursMicro," jours et a=",a)
+        x = NbreDeJoursMicro
+        y = a
+        NbreDeJoursMicro = NbreDeJoursMicro+1
+    if x == 179:
         return True
 
-levier=int(input("Entrez un levier de 3, 5, 10, 20 ou 100 : "))
-liquidation=funcLevier(levier)
+levier = int(input("Pick a leverage of 3, 5, 10, 20 or 100 : "))
+liquidation = funcLevier(levier)
 
 while NbreDeJoursMacro<400:
     if jeff(NbreDeJoursMacro):
         listeBingo.append(NbreDeJoursMacro)
-    NbreDeJoursMacro=NbreDeJoursMacro+1
-print("Voici la liste des increments des dates valides a partir du ", dateDepartFixe," : ",listeBingo)
+    NbreDeJoursMacro = NbreDeJoursMacro+1
+print("Here is the list of the increments of the valid dates for your position from the ", dateDepartFixe,"with your selected leverage : ",listeBingo)
 
